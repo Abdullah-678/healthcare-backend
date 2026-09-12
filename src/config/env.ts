c47ae490f1689483b9a5fca2df9876d730c64a48
Abdullah-678 <abdullah37822@gmail.com>
2026-09-12 21:53:39 +0600

    stripe setup

--- src/config/env.ts
+++ src/config/env.ts
@@ -30,6 +30,10 @@ interface EnvConfig {
     CLOUDINARY_API_KEY: string;
     CLOUDINARY_SECRET: string;
   };
+  STRIPE: {
+    STRIPE_SECRET_KEY: string;
+    STRIPE_WEBHOOK_SECRET: string;
+  };
 }
 
 const loadEnvVariables = (): EnvConfig => {
@@ -57,6 +61,8 @@ const loadEnvVariables = (): EnvConfig => {
     "CLOUDINARY_CLOUD_NAME",
     "CLOUDINARY_API_KEY",
     "CLOUDINARY_SECRET",
+    "STRIPE_SECRET_KEY",
+    "STRIPE_WEBHOOK_SECRET",
   ];
 
   requiredEnvVariable.forEach((variable) => {
@@ -95,6 +101,10 @@ const loadEnvVariables = (): EnvConfig => {
       CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
       CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
       CLOUDINARY_SECRET: process.env.CLOUDINARY_SECRET as string,
+    },
+    STRIPE: {
+      STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY as string,
+      STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET as string,
     },
   };
 };
